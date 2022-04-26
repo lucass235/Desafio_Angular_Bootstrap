31ecb3940b081f0211ab1607de6d7743dc7e8328
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2022-04-26 12:31:18 -0300

    .

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,11 +1,3 @@
-// export class User {
-
-//   constructor(public email: string, public password: string) {
-//     this.email = email;
-//     this.password = password;
-//   }
-// }
-
 export class User {
   email?: string;
   password?: string;
